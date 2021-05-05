1c0e424eeedefb307052a6febd3169eb62a6349c
cyb <cliffbakalian@gmail.com>
2021-05-05 12:53:14 -0400

    cleaned up mnist

--- sisa/datasets/mnist/dataloader.py
+++ sisa/datasets/mnist/dataloader.py
@@ -5,17 +5,6 @@ from urllib import request
 import gzip
 pwd = os.path.dirname(os.path.realpath(__file__))
 
-train_data = np.load(os.path.join(pwd, 'purchase2_train.npy'), allow_pickle=True)
-test_data = np.load(os.path.join(pwd, 'purchase2_test.npy'), allow_pickle=True)
-
-train_data = train_data.reshape((1,))[0]
-test_data = test_data.reshape((1,))[0]
-
-X_train = train_data['X'].astype(np.float32)
-X_test = test_data['X'].astype(np.float32)
-y_train = train_data['y'].astype(np.int64)
-y_test = test_data['y'].astype(np.int64)
-
 # lets just hack some stuff together. taken from mnist.py
 # https://github.com/hsjeong5/MNIST-for-Numpy
 def mnist():
